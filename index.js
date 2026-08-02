8e67e706d9a5e167aa0d0105a619ba074100e752
Khubaib Khawaja <khubaibqadri789@gmail.com>
2026-08-02 12:31:25 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let tempOptions = document.querySelectorAll(".option1");
+let windSpeedOptions = document.querySelectorAll(".option2");
+let precipitationOptions = document.querySelectorAll(".option3");
+
+function toggleSelection(options, clickedOption) {
+  options.forEach((opt) => {
+    opt.querySelector("i")?.classList.add("hide");
+    opt.classList.remove("bg");
+  });
+
+  clickedOption.querySelector("i")?.classList.remove("hide");
+  clickedOption.classList.add("bg");
+}
+
+tempOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    toggleSelection(tempOptions, option);
+  });
+});
+
+windSpeedOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    toggleSelection(windSpeedOptions, option);
+  });
+});
+
+precipitationOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    toggleSelection(precipitationOptions, option);
+  });
+});
